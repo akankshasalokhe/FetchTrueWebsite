cb38f690a4ee5abc9cf9bf9b38da2254a389e4c5
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-12-04 10:22:55 +0530

    create header and navbar section

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,19 @@
 import Navbar from "@/src/components/Navbar/Navbar";
 import "./globals.css";
+import { ReactNode } from "react";
 
-export default function RootLayout({ children }) {
+export const metadata = {
+  title: "Fetch True",
+  description: "Your App Description",
+};
+
+
+export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
       <body>
         <Navbar />
-        <main className="">{children}</main>
-        
+        <main>{children}</main>
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import HeaderSection from "@/src/components/Header/Header";
+import Modules from "@/src/components/Modules/Modules";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <>
      <HeaderSection />
+     <Modules />
    </>
   );
 }
