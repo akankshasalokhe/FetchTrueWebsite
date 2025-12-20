6ec82e4e4c53569bd308b73d9936bde4ec968feb
akankshasalokhe <akankshasalokhe2001@gmail.com>
2025-12-20 17:24:41 +0530

    modified in home  page

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import HeaderSection from "@/src/components/Header/Header";
 import Modules from "@/src/components/Modules/Modules";
 import Refer from "@/src/components/Refer/Refer";
 import BestOffers from "@/src/components/Modules/BestOffers";
-import RecentServices from "@/src/components/Modules/RecentServices";
-import Recommendation from "@/src/components/Modules/Recommendation";
 import GrowthPartner from "@/src/components/GrowthPartner/GrowthPartner";
 import TopTrending from "@/src/components/TopTrending/TopTrending";
 import Navbar from "@/src/components/Navbar/Navbar";
